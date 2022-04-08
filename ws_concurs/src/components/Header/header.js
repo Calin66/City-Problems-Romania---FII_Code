75cb8974939015fec5ebfed8b116168c9d0ddc6a
Calin66 <andreicalinciobanu@gmail.com>
2022-04-08 12:29:21 +0300

    Header_navbar v2

--- ws_concurs/src/components/Header/header.js
+++ ws_concurs/src/components/Header/header.js
@@ -1,33 +1,20 @@
-import React from 'react';
-import imgheader from "./../images/poza_header.jpg";
+import React from "react";
+import header1 from "./../images/header1.mp4";
 import "./header.css";
 const Header = () => {
   return (
     <div>
-        <img src={imgheader} id="imgh"/>
-        <div id="transp"></div>
-        <div id="infoplus">
-          <h1>Intrebari, propuneri si probleme</h1>
-          <p>Fa tot ce poti pentru o Romanie mai buna!</p>
-          <button id="button">Posteaza si tu!</button>
-          <div id="infoplusop"></div>
-        </div>
-        <div id="postari_filtrare">
-          <div>
-            Postari
-          </div>
-          <div>
-            Filtrare
-          </div>
-          <div style={{backgroundColor:"red"}}>
-
-          </div>
-          <div style={{backgroundColor:"orange"}}>
-
-          </div>
-        </div>
+      <video src={header1} loop muted autoPlay />
+      <div id="transp"></div>
+      <div id="infoplus">
+        <h1>City Problems Romania</h1>
+        <h2>Intrebari, propuneri si probleme</h2>
+        <p>Fa tot ce poti pentru o Romanie mai buna!</p>
+        <button id="button">Posteaza si tu!</button>
+        <div id="infoplusop"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
