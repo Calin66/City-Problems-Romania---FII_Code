de7867b3eac7636ea894bf53d1d24512b1573307
Calin66 <62961099+Calin66@users.noreply.github.com>
2022-05-03 04:37:56 -0700

    test

---
+++ ws_concurs/src/components/Pages/Postare/validateAP.js
@@ -0,0 +1,19 @@
+export default function validateAP(values) {
+    let errors = {};
+    if (!values.titlu.trim()) {
+      errors.titlu = "Camp obligatoriu";
+    }
+    if(!values.tproblema){
+      errors.tproblema = "Camp obligatoriu";
+    }
+    if(!values.pozeVideo){
+      errors.pozeVideo = "Camp obligatoriu";
+    }
+    if(!values.descriere){
+      errors.descriere = "Camp obligatoriu";
+    }
+    if(!values.grad){
+      errors.grad = "Camp obligatoriu";
+    }
+    return errors;
+}
