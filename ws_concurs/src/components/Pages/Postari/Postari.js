59727a007565ed2b842d595c4536131caf9c6adc
Calin66 <62961099+Calin66@users.noreply.github.com>
2022-05-07 06:41:59 +0300

    imagini firestore

--- ws_concurs/src/components/Pages/Postari/Postari.js
+++ ws_concurs/src/components/Pages/Postari/Postari.js
@@ -5,33 +5,24 @@ import Navbar from '../../NavBar/navBar';
 import "./Postari.css";
 
 const Postari = () => {
-    const [postareCData, setPostareCData] = useState();
-    const getPostari = async () => {
-        const querySnapshot = await getDocs(collection(db, "posts"));
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.data());
-            setPostareCData(doc.data());
-        });
-        // console.log(postareCData);
-        // const postariCollectionRef = doc(db, "postari");
-        // const docSnap = await getDoc(postariCollectionRef);
-        // if (docSnap.exists()) {
-            // setPostareCData(docSnap.data());
-        // } else {
-            // console.log("No");
-        // }
-    }
-    
+    const postsCollectionRef = collection(db, "posts");
+    const [posts, setPosts] = useState([]);
+
     useEffect(() => {
-        getPostari();
-      },[]);
+        const getPosts = async () => {
+          const data = await getDocs(postsCollectionRef);
+          setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
+        getPosts();
+      }, []);
+
     return (
         <div style={{display:"flex", alignItems:"center", flexDirection:"column"}}>
             <Navbar backgroundColor="black"/>
             <div className='container-postari'>
-                <div className='container-postare'><h1>{postareCData && postareCData.titlu}</h1></div>
-                <h1></h1>
+                {posts.map(post => {
+                    return <div className='container-postare'><h1>{post.titlu}</h1> <p>{post.descriere}</p></div>
+                })}
             </div>
         </div>
     )
